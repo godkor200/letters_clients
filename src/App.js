d19cc39a303446798caaf9cf019a57d49948003d
YUBYEONGGUK <godkor200@gmail.com>
2020-12-30 23:20:14 +0900

    fix route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from "react";
-import Login from "./Component/Login";
-import LetterEntry from "./Component/LetterEntry";
+import Login from "./component/Login";
+import LetterEntry from "./component/LetterEntry";
 import "./App.css";
 
 const App = () => {
