9a29220e81c156eec990a4e7b8cd7f4aa18c5dea
YUBYEONGGUK <godkor200@gmail.com>
2020-12-30 19:44:32 +0900

    fix component-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import Letters from "./Component/Letters";
-import LetterAdd from "./Component/LetterAdd";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import Login from "./Component/Login";
+import Index from "./Component/Index";
 import "./App.css";
-import { Helmet } from "react-helmet";
 
-function App() {
+const App = () => {
   return (
-    <div className="body-warpper">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>수빈이 한태 편지쓰기</title>
-      </Helmet>
-      <LetterAdd />
-      <Letters />
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/letter" component={Index} />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
