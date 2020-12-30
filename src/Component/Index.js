f94542bd21cc0c30512e5441b6d459e2008db8ef
YUBYEONGGUK <godkor200@gmail.com>
2020-12-30 19:45:07 +0900

    Move File

---
+++ src/Component/Index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Letters from "./Letters";
+import LetterAdd from "./LetterAdd";
+import "../App.css";
+import { Helmet } from "react-helmet";
+const Index = () => {
+  return (
+    <div className="body-warpper">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>편지쓰기</title>
+      </Helmet>
+      <LetterAdd />
+      <Letters />
+    </div>
+  );
+};
+export default Index;
