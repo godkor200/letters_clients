3b2197b1149933d9db54be9e8a303a9e2ef78e6e
YUBYEONGGUK <godkor200@gmail.com>
2021-01-03 19:12:17 +0900

    Modify Dependency Relationships

--- src/Component/LetterEntry.js
+++ src/Component/LetterEntry.js
@@ -2,15 +2,9 @@ import React from "react";
 import Letters from "./Letters";
 import LetterAdd from "./LetterAdd";
 import "../App.css";
-import { Helmet } from "react-helmet";
 const LetterEntry = () => {
   return (
     <div className="body-warpper">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>편지쓰기</title>
-      </Helmet>
-      <LetterAdd />
       <Letters />
     </div>
   );
